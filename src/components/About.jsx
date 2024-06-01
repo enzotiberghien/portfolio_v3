5984d592c25eae885bf07ab07a32f244a8090a18
enzotiberghien <enzotiberghien28@gmail.com>
2024-06-01 11:17:52 +0200

    updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,7 @@ const About = () => {
   return (
     <div className='space-y-12' id='about'>
       <h2 className='text-2xl md:text-3xl font-medium'>About me <span className='text-orange'>/</span></h2>
-      <p className='md-6/6 md:w-4/6 font-sans'>Hello! I'm Enzo Tiberghien, a passionate and dedicated Full Stack Developer. I have recently graduated and am eager to embark on my professional journey in the tech industry. My strongest skills lie in JavaScript, but I am also proficient in other languages such as Java and C#.
-        Before pursuing a career in coding, I worked in restaurants, where I honed my skills in teamwork, communication, and time management. These experiences have shaped me into a well-rounded individual ready to tackle challenges in the tech world. I am now actively seeking opportunities to apply my skills and grow as a developer.
+      <p className='md-6/6 md:w-4/6 font-sans'>Hello! I'm Enzo Tiberghien, a passionate and dedicated Full Stack Developer who recently graduated from Kristianstad University. I am eager to embark on my professional journey in the tech industry, bringing a strong foundation in software development and a commitment to continuous learning. I am actively seeking opportunities to apply my knowledge and grow as a developer.
       </p>
     </div>
   )
